6678e4220b88f1c873079d2343128b418ad95c74
Muhammed-Sirajudeen <muhammedsirajudeen29@gmail.com>
2024-09-11 18:28:50 +0530

    pagination added at all implemented listings

--- Frontend/src/pages/User/Course.tsx
+++ Frontend/src/pages/User/Course.tsx
@@ -8,13 +8,17 @@ import { setPage } from "../../store/globalSlice";
 
 export default function Course(): ReactElement {
   const [courses, setCourses] = useState<Array<courseProps>>([]);
-  const dispatch=useAppDispatch()
+  const [currentpage, setCurrentpage] = useState<number>(1);
+  const [pagecount, setPagecount] = useState<number>(0);
+
+  const dispatch = useAppDispatch();
   const navigate = useNavigate();
+
   useEffect(() => {
-    dispatch(setPage("course"))
+    dispatch(setPage("course"));
     async function dataWrapper() {
       const response = (
-        await axios.get(url + "/user/course", {
+        await axios.get(url + `/user/course?page=${currentpage}`, {
           headers: {
             Authorization: `Bearer ${window.localStorage.getItem("token")}`,
           },
@@ -22,9 +26,10 @@ export default function Course(): ReactElement {
       ).data;
       console.log(response);
       setCourses(response.courses);
+      setPagecount(response.pageLength);
     }
     dataWrapper();
-  }, [dispatch]);
+  }, [dispatch,currentpage]);
   const courseNavHandler = () => {
     navigate("/user/roadmap");
   };
@@ -32,10 +37,34 @@ export default function Course(): ReactElement {
     e.stopPropagation();
     alert("clicked here");
   };
-  const favHandler=(e:React.MouseEvent<HTMLImageElement>)=>{
-    e.stopPropagation()
-    alert("favorite clicked")
-  }
+  const favHandler = (e: React.MouseEvent<HTMLImageElement>) => {
+    e.stopPropagation();
+    alert("favorite clicked");
+  };
+  const pageHandler = (count: number) => {
+    const page = Math.ceil(count / 10);
+    const array = [];
+    for (let i = 0; i < page; i++) {
+      array.push(i + 1);
+    }
+    return array;
+  };
+  const previouspageHandler = () => {
+    const prev = currentpage - 1;
+    if (prev <= 0) {
+      setCurrentpage(1);
+      return;
+    }
+    setCurrentpage(prev);
+  };
+  const nextpageHandler = () => {
+    const next = currentpage + 1;
+    if (next > Math.ceil(pagecount / 10)) {
+      setCurrentpage(Math.ceil(pagecount / 10));
+      return;
+    }
+    setCurrentpage(next);
+  };
   return (
     <div className="ml-36 flex justify-evenly flex-wrap mt-16">
       {courses.map((course) => {
@@ -50,7 +79,11 @@ export default function Course(): ReactElement {
               src={`${url}/course/${course.courseImage}`}
             />
             <div className="flex justify-between w-full">
-            <img onClick={favHandler} src="/course/favorite.png" className="h-5 w-5" />
+              <img
+                onClick={favHandler}
+                src="/course/favorite.png"
+                className="h-5 w-5"
+              />
               <img
                 onClick={(e) => courseModalHandler(e)}
                 src="/ellipsis.png"
@@ -66,6 +99,34 @@ export default function Course(): ReactElement {
           </div>
         );
       })}
+      <div className="fixed left-1/2 bottom-10  w-screen flex">
+        <div className="flex items-center justify-evenly w-32">
+          <button
+            onClick={previouspageHandler}
+            className="flex h-8 items-center justify-center"
+          >
+            <img src="/course/prev.png" className="h-6" />
+          </button>
+          {pageHandler(pagecount).map((page) => {
+            return (
+              <button
+                key={page}
+                className={`border border-black p-2 rounded-xl h-8 flex items-center justify-center text-xs ${
+                  currentpage === page ? "bg-black text-white" : ""
+                } `}
+              >
+                {page}
+              </button>
+            );
+          })}
+          <button
+            onClick={nextpageHandler}
+            className="flex h-8 items-center justify-center"
+          >
+            <img src="/course/next.png" className="h-6" />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
