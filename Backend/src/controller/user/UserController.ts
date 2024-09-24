e91c8d9d358d2f9abfec916e48ac6479e072b715
Muhammed-Sirajudeen <muhammedsirajudeen29@gmail.com>
2024-09-24 17:40:01 +0530

    feat: improved profile backend

--- Backend/src/controller/user/UserController.ts
+++ Backend/src/controller/user/UserController.ts
@@ -1,35 +1,23 @@
 import { Request, Response } from 'express';
-import User from '../../model/User';
-interface userProps {
-  email: string;
-  password: string;
-  profileImage: string;
-}
+import User, { IUser } from '../../model/User';
+
 const backendUrl = 'http://localhost:3000/';
 
 const UserUpdate = async (req: Request, res: Response) => {
   try {
-    const user = req.user as userProps;
+    const user = req.user as IUser;
     const { phone, address, profileImage, email } = req.body;
-    const checkUser = await User.findOne({ email });
+    // const checkUser = await User.findOne({ email });
     let filename;
     if (req.file?.filename) {
       filename = req.file.filename;
-    } else if (checkUser?.profileImage) {
-      filename = checkUser.profileImage;
+    } else if (user?.profileImage) {
+      filename = user.profileImage;
     }
-    const updateUser = await User.updateOne(
-      {
-        email: user.email,
-      },
-      {
-        phone: phone,
-        address: address,
-        profileImage:
-          filename ??
-          'https://img.icons8.com/ios-glyphs/30/1A1A1A/user--v1.png',
-      }
-    );
+    user.phone=phone
+    user.address=address
+    user.profileImage=filename ?? 'https://img.icons8.com/ios-glyphs/30/1A1A1A/user--v1.png',
+    await user.save()
     res.status(200).json({ message: 'success' });
   } catch (error) {
     console.log(error);
