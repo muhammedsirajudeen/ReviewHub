f27ff46dab1c04f0e1d3b524c5cfd156417a42ab
Muhammed-Sirajudeen <muhammedsirajudeen29@gmail.com>
2024-10-05 23:26:22 +0530

    feat: base for notification module

--- Frontend/src/pages/User/Notification.tsx
+++ Frontend/src/pages/User/Notification.tsx
@@ -1,17 +1,17 @@
-import { ReactElement, useEffect } from "react";
-import { useAppDispatch } from "../../store/hooks";
-import { setPage } from "../../store/globalSlice";
-import DashboardTopbar from "../../components/DashboardTopbar";
+import { ReactElement, useEffect } from 'react';
+import { useAppDispatch } from '../../store/hooks';
+import { setPage } from '../../store/globalSlice';
+import DashboardTopbar from '../../components/DashboardTopbar';
 
-export default function Notification():ReactElement{
-    const dispatch=useAppDispatch()
-    useEffect(()=>{
-        dispatch(setPage('notification'))
-    },[dispatch])
-    return(
-        <>
-        <DashboardTopbar/>
-        <h1 className="ml-36 text-3xl mt-10" >NOTIFICATIONS</h1>
-        </>
-    )
+export default function Notification(): ReactElement {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(setPage('notification'));
+  }, [dispatch]);
+  return (
+    <>
+      <DashboardTopbar />
+      <h1 className="ml-36 text-3xl mt-10">NOTIFICATIONS</h1>
+    </>
+  );
 }
