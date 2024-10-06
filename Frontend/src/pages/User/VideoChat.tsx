286a7f606e951ddd94ee6a21796c6a7cab181365
Muhammed-Sirajudeen <muhammedsirajudeen29@gmail.com>
2024-10-06 17:30:00 +0530

    feat: formatting on video calling module

--- Frontend/src/pages/User/VideoChat.tsx
+++ Frontend/src/pages/User/VideoChat.tsx
@@ -1,5 +1,5 @@
 import { ReactElement, useEffect, useRef, useState } from 'react';
-import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import { useAppDispatch } from '../../store/hooks';
 import { setPage } from '../../store/globalSlice';
 import { useLoaderData, useLocation, useNavigate } from 'react-router';
 import { ClipLoader } from 'react-spinners';
@@ -16,9 +16,8 @@ export default function VideoChat(): ReactElement {
   const [loading, setLoading] = useState<boolean>(true);
   const user = useLoaderData() as userProps;
   const streamRef = useRef<MediaStream>();
-  const remoteVideoRef=useRef<HTMLVideoElement>(null)
+  const remoteVideoRef = useRef<HTMLVideoElement>(null);
   useEffect(() => {
-    let mediaStream:MediaStream
     async function reviewDetailsFetcher() {
       try {
         const response = (
@@ -34,27 +33,27 @@ export default function VideoChat(): ReactElement {
     }
     dispatch(setPage('review'));
     async function mediaStreamFetcher(): Promise<MediaStream> {
-        // Check if the stream is already instantiated
-        if (streamRef.current) {
-            console.log("Returning existing media stream");
-            return streamRef.current; // Return existing stream
-        }
-        
-        try {
-            // If the stream is not instantiated, get a new one
-            const stream = await navigator.mediaDevices.getUserMedia({
-                video: true,
-                audio: true,
-            });
-            streamRef.current = stream; // Store the new stream in ref
-            return stream;
-        } catch (error) {
-            console.error("Error accessing media devices:", error);
-            throw error; // Rethrow error for handling in the calling function if needed
-        }
+      // Check if the stream is already instantiated
+      if (streamRef.current) {
+        console.log('Returning existing media stream');
+        return streamRef.current; // Return existing stream
+      }
+
+      try {
+        // If the stream is not instantiated, get a new one
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+          audio: true,
+        });
+        streamRef.current = stream; // Store the new stream in ref
+        return stream;
+      } catch (error) {
+        console.error('Error accessing media devices:', error);
+        throw error; // Rethrow error for handling in the calling function if needed
+      }
     }
     async function requestPermission() {
-      const stream = await mediaStreamFetcher()
+      const stream = await mediaStreamFetcher();
       if (localVideoRef.current) {
         try {
           localVideoRef.current.srcObject = stream;
@@ -72,31 +71,28 @@ export default function VideoChat(): ReactElement {
       port: 3000,
       path: '/peerjs/myapp', // The path you defined in the server
       secure: false,
-      debug:3
+      debug: 3,
     });
 
     peer.on('open', async (id) => {
       console.log('My peer ID is: ' + id);
       const callerId = window.sessionStorage.getItem('callerId');
-      const stream=await mediaStreamFetcher()
-      if(!stream){
-        console.log("No media stream provided")
+      const stream = await mediaStreamFetcher();
+      if (!stream) {
+        console.log('No media stream provided');
       }
       try {
-        const call = await peer.call(
-          callerId as string,
-          stream
-        );
-        call.on('stream',(remoteStream)=>{
-            if (remoteVideoRef.current) {
-                remoteVideoRef.current.srcObject = remoteStream;
-                remoteVideoRef.current.play();
-                setLoading(false)
-              }
-        })
-        
+        const call = await peer.call(callerId as string, stream);
+        call.on('stream', (remoteStream) => {
+          if (remoteVideoRef.current) {
+            remoteVideoRef.current.srcObject = remoteStream;
+            remoteVideoRef.current.play();
+            setLoading(false);
+          }
+        });
+
         if (!call) {
-          console.log('Connection Unsuccessful')
+          console.log('Connection Unsuccessful');
           return;
         }
       } catch (error) {
@@ -104,20 +100,18 @@ export default function VideoChat(): ReactElement {
       }
     });
     peer.on('call', async (call) => {
-      call.answer(await mediaStreamFetcher())
+      call.answer(await mediaStreamFetcher());
       call.on('stream', (remoteStream) => {
         if (remoteVideoRef.current) {
           remoteVideoRef.current.srcObject = remoteStream;
           remoteVideoRef.current.play();
-          setLoading(false)
+          setLoading(false);
         }
       });
-
     });
     return () => {
       peer.disconnect();
-      peer.destroy();    // Destroy the peer instance
-
+      peer.destroy(); // Destroy the peer instance
     };
   }, [dispatch, location, user._id]);
   const backHandler = () => {
@@ -141,7 +135,10 @@ export default function VideoChat(): ReactElement {
           </video>
         </div>
         <div className="w-1/2 ml-20 flex items-center justify-start ">
-          <video ref={remoteVideoRef} className="w-3/4 h-3/4 bg-black rounded-xl"></video>
+          <video
+            ref={remoteVideoRef}
+            className="w-3/4 h-3/4 bg-black rounded-xl"
+          ></video>
         </div>
       </div>
       <button className="absolute rounded-lg p-2 items-center justify-center bottom-10 left-1/2 bg-red-600 flex ">
