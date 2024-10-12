030962d113f5c9a3c796a1a7ca5bb39d45028b1a
Muhammed-Sirajudeen <muhammedsirajudeen29@gmail.com>
2024-10-12 09:44:44 +0530

    Feat: formatting issue

--- Backend/src/controller/user/CourseController.ts
+++ Backend/src/controller/user/CourseController.ts
@@ -8,15 +8,15 @@ export const PAGE_LIMIT = 10;
 interface dateProps {
   $gt: Date;
 }
-interface favoriteProps{
-  $in:mongoose.Types.ObjectId[]
+interface favoriteProps {
+  $in: mongoose.Types.ObjectId[];
 }
 interface queryProps {
   domain?: string;
   postedDate?: dateProps;
   courseName?: RegExp;
   unlistStatus?: boolean;
-  _id?:favoriteProps
+  _id?: favoriteProps;
 }
 
 //constructing the query as it goes
@@ -26,7 +26,7 @@ const CourseList = async (req: Request, res: Response) => {
     let { page } = req.query ?? '1';
     const { domain } = req.query;
     const { date, search, favorite } = req.query;
-    console.log(user.favoriteCourses)
+    console.log(user.favoriteCourses);
     const newDate: Date = new Date(date as string);
     const query: queryProps = {};
     if (user.authorization !== 'admin') {
@@ -35,8 +35,8 @@ const CourseList = async (req: Request, res: Response) => {
     if (domain) query.domain = domain as string;
     if (date) query.postedDate = { $gt: newDate };
     if (search) query.courseName = new RegExp(search as string, 'i');
-    if(favorite){
-      query._id={$in:user.favoriteCourses}
+    if (favorite) {
+      query._id = { $in: user.favoriteCourses };
     }
     const length = (await Course.find(query)).length;
     const Courses = await Course.find(query)
