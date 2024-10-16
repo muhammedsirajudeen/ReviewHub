fef0c26eada6924accfe0f348c5dc312bec848de
muhammedsirajudeen <muhammedsirajudeen29@gmail.com>
2024-10-17 00:05:31 +0300

    Feat: testing ci cd

--- Backend/src/helper/bcryptHelper.ts
+++ Backend/src/helper/bcryptHelper.ts
@@ -3,7 +3,6 @@ async function hashPassword(plainTextPassword: string): Promise<string> {
   try {
     // Generate salt
     const salt = await bcrypt.genSalt(10);
-
     // Hash the password with the generated salt
     const hash = await bcrypt.hash(plainTextPassword, salt);
 
