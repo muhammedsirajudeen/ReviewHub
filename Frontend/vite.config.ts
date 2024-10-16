2abbaa83da9425818af38972f8aa9aeceac55cbe
muhammedsirajudeen <muhammedsirajudeen29@gmail.com>
2024-10-16 20:45:27 +0300

    Feat: minor bug fix

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     host:'0.0.0.0',
-    port:5173
+    port: process.env.PORT as unknown as number || 5173,
   },
   plugins: [react()],
 })
